81bb41e1bab7a0bc1c9b8e80b69bd3e348f7dabf
omarkar101 <omar.kar101@gmail.com>
2021-05-01 03:02:46 +0300

    Added Companies and Edit if companies

--- Company/Controllers/CompaniesController.cs
+++ Company/Controllers/CompaniesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Company.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,56 @@ namespace Company.Controllers
 {
     public class CompaniesController : Controller
     {
+        private readonly CompanyContext _db;
+
+        public CompaniesController(CompanyContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            var companies = _db.Companies.ToList();
+            ViewData["Companies"] = companies;
             return View();
+        }
+
+        public IActionResult Edit(string? name)
+        {
+            if (name == null)
+            {
+                return NotFound();
+            }
+
+            var company = _db.Companies.Where(c => c.Name.Equals(name)).FirstOrDefault();
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return View(company);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(string name, [Bind("Name,Country,City,PhoneNumber,ContactEmail")] Models.Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _db.Update(company);
+                    _db.SaveChanges();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e);
+                    return NotFound();
+                }
+                return RedirectToAction("Index", "Companies");
+            }
+            return View(company);
         }
     }
 }
