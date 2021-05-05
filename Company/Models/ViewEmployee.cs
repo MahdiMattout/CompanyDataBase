bb98e0978ea338709ced9fe42136faa057509c38
Mahdi Mattout <mahdimattout04@gmail.com>
2021-05-05 19:04:59 +0300

    Creating MVC for a View

---
+++ Company/Models/ViewEmployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Company.Models
+{
+    public partial class ViewEmployee
+    {
+        public ViewEmployee()
+        {
+            CompanyPcs = new HashSet<CompanyPc>();
+            Relatives = new HashSet<Relative>();
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int? OfficeExtension { get; set; }
+        public int? OfficeNumber { get; set; }
+        public string Position { get; set; }
+        public string CompanyName { get; set; }
+
+        public virtual Company CompanyNameNavigation { get; set; }
+        public virtual HourlyPaid HourlyPaid { get; set; }
+        public virtual MonthlyPaid MonthlyPaid { get; set; }
+        public virtual ICollection<CompanyPc> CompanyPcs { get; set; }
+        public virtual ICollection<Relative> Relatives { get; set; }
+    }
+}
